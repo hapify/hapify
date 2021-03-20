9b3e7b37b253044a113bb6679da58596a41b43fc
Edouard Demotes-Mainard <e.demotes@gmail.com>
2021-03-20 10:11:50 -0400

    ci: deployment to github pages

---
+++ docs/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+DIST_DIR="${DIR}/dist"
+
+# Build main language
+cd "${DIR}/fr" || exit 1
+mkdocs build --clean --site-dir "${DIST_DIR}"
+
+# Build other languages
+cd "${DIR}/fr" || exit 1
+mkdocs build --dirty --site-dir "${DIST_DIR}/fr"
+
+# Deploy on github pages
+cd "${DIR}" || exit 1
+ghp-import \
+  --cname documentation.hapify.io \
+  --remote pages \
+  --branch master \
+  --push \
+  "${DIST_DIR}"

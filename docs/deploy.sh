0328dca246c703afaaa9639f097794c6c8121239
Edouard Demotes-Mainard <e.demotes@gmail.com>
2021-03-21 22:02:37 -0400

    ci: change deployment config

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 DIST_DIR="${DIR}/dist"
 
 # Build main language
-cd "${DIR}/fr" || exit 1
+cd "${DIR}/en" || exit 1
 mkdocs build --clean --site-dir "${DIST_DIR}"
 
 # Build other languages
@@ -14,7 +14,7 @@ mkdocs build --dirty --site-dir "${DIST_DIR}/fr"
 # Deploy on github pages
 cd "${DIR}" || exit 1
 ghp-import \
-  --cname documentation.hapify.io \
+  --cname docs.hapify.io \
   --remote pages \
   --branch master \
   --push \

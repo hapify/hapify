179145c1cd41fb91577d333905b579ad772084e1
Edouard Demotes <e.demotes@gmail.com>
2021-09-15 01:46:22 -0400

    chore: add config interfaces

---
+++ packages/core/src/Generate.ts
@@ -0,0 +1,15 @@
+
+export async function  generate(): Promise<void> {
+
+  // Load config
+
+  // Get models
+
+  // Get templates
+
+  // Get context creators required by templates
+
+  // Create contexts from each context creators
+
+
+}
